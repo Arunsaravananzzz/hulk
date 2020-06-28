be914f572387d4b48d777413f900c3fd53037d9d
saravanan <saravanan_vertace@yahoo.com>
2020-06-28 13:33:13 +0530

    remove unwanted details

--- server.js
+++ server.js
@@ -13,19 +13,6 @@ console.log("Server is listening");
 
 
 
-endee-13-6kl
-20litre-16
-
-
-
-frc-7-6kl,,,MASAND(NICE)--3*80====Rs.60/cft
-
-
-endee
-14-1-20--->360--14-01-20
-14-1-20---->340
----------------
-	700Nos
 
 
 
