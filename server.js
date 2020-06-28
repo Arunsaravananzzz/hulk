75ef2d52d26fcfdea72246776884a901d88a09e8
saravanan <saravanan_vertace@yahoo.com>
2020-06-28 13:29:57 +0530

    add additional frame in node,html page

---
+++ server.js
@@ -0,0 +1,32 @@
+var http = require("http");
+http.createServer(function(req, res) {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end('Hello q\n');
+}).listen(8080,'127.0.0.1');
+
+console.log("Server is listening");
+
+
+
+
+
+
+
+
+endee-13-6kl
+20litre-16
+
+
+
+frc-7-6kl,,,MASAND(NICE)--3*80====Rs.60/cft
+
+
+endee
+14-1-20--->360--14-01-20
+14-1-20---->340
+---------------
+	700Nos
+
+
+
+
